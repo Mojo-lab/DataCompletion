12084960f4f853c675f6d82b9f3351ec1c1b36e1
Hariharan Vijayan <hariharan@Hariharans-MacBook-Air.local>
2023-03-03 06:11:07 +0530

    updated files

--- dataCompletion.py
+++ dataCompletion.py
@@ -22,6 +22,30 @@ def homepage():
             else:
                 categorical_features.append(feature)
 
+                def mean(data_pure, numerical_features):
+                    try:
+                        for i in numerical_features:
+                            std_before_mean_replacement = np.round(data_pure[i].std(), 4)
+                            data_pure[i].fillna(data_pure[i].mean(), inplace=True)
+                            std_After_mean_replacement = np.round(data_pure[i].std(), 4)
+                            print(
+                                "replaced the feature {} with mean and the standard deviation befor applying mean {} and after applying mean is: {}".format(
+                                    i, std_before_mean_replacement, std_After_mean_replacement))
+                    except:
+                        raise Exception
+
+                def median(data_pure, numerical_features):
+                    try:
+                        for i in numerical_features:
+                            std_before_median_replacement = np.round(data_pure[i].std(), 4)
+                            data_pure[i].fillna(data_pure[i].median(), inplace=True)
+                            std_After_median_replacement = np.round(data_pure[i].std(), 4)
+                            print(
+                                "replaced the feature {} with median and the standard deviation befor applying median {} and after applying median is: {}".format(
+                                    i, std_before_median_replacement, std_After_median_replacement))
+                    except:
+                        raise Exception
+
 
 if __name__ == '__main__':
     app.debug = True
