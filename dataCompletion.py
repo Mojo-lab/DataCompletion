1a3e74901c72038f0648ab7382ef48f5e63bb79f
Hariharan Vijayan <hariharan@Hariharans-MacBook-Air.local>
2023-03-03 06:46:57 +0530

    updated files

--- dataCompletion.py
+++ dataCompletion.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     import pandas as pd
+    import numpy as np
+
     data_pure = pd.read_csv('titanic.csv')
     features_with_nan = [feature for feature in data_pure.columns if data_pure[feature].isnull().sum() >= 1]
     missing_percentage = []
@@ -22,30 +24,57 @@ def homepage():
             else:
                 categorical_features.append(feature)
 
-                def mean(data_pure, numerical_features):
-                    try:
-                        for i in numerical_features:
-                            std_before_mean_replacement = np.round(data_pure[i].std(), 4)
-                            data_pure[i].fillna(data_pure[i].mean(), inplace=True)
-                            std_After_mean_replacement = np.round(data_pure[i].std(), 4)
-                            print(
-                                "replaced the feature {} with mean and the standard deviation befor applying mean {} and after applying mean is: {}".format(
-                                    i, std_before_mean_replacement, std_After_mean_replacement))
-                    except:
-                        raise Exception
+                class numerical_feature():
+                    def mean(data_pure, numerical_features):
+                        try:
+                            for i in numerical_features:
+                                std_before_mean_replacement = np.round(data_pure[i].std(), 4)
+                                data_pure[i].fillna(data_pure[i].mean(), inplace=True)
+                                std_After_mean_replacement = np.round(data_pure[i].std(), 4)
+                                print(
+                                    "replaced the feature {} with mean and the standard deviation befor applying mean {} and after applying mean is: {}".format(
+                                        i, std_before_mean_replacement, std_After_mean_replacement))
+                        except:
+                            raise Exception
 
-                def median(data_pure, numerical_features):
-                    try:
-                        for i in numerical_features:
-                            std_before_median_replacement = np.round(data_pure[i].std(), 4)
-                            data_pure[i].fillna(data_pure[i].median(), inplace=True)
-                            std_After_median_replacement = np.round(data_pure[i].std(), 4)
-                            print(
-                                "replaced the feature {} with median and the standard deviation befor applying median {} and after applying median is: {}".format(
-                                    i, std_before_median_replacement, std_After_median_replacement))
-                    except:
-                        raise Exception
+                    def median(data_pure, numerical_features):
+                        try:
+                            for i in numerical_features:
+                                std_before_median_replacement = np.round(data_pure[i].std(), 4)
+                                data_pure[i].fillna(data_pure[i].median(), inplace=True)
+                                std_After_median_replacement = np.round(data_pure[i].std(), 4)
+                                print(
+                                    "replaced the feature {} with median and the standard deviation befor applying median {} and after applying median is: {}".format(
+                                        i, std_before_median_replacement, std_After_median_replacement))
+                        except:
+                            raise Exception
 
+                    def mode(data_pure, numerical_features):
+                        try:
+                            for i in numerical_features:
+                                std_before_mode_replacement = np.round(data_pure[i].std(), 4)
+                                data_pure[i].fillna(data_pure[i].mode()[0], inplace=True)
+                                std_After_mode_replacement = np.round(data_pure[i].std(), 4)
+                                print(
+                                    "replaced the feature {} with mode and the standard deviation befor applying mode {} and after applying mode is: {}".format(
+                                        i, std_before_mode_replacement, std_After_mode_replacement))
+                        except:
+                            raise Exception
+
+                    def random_sample(data_pure, numerical_features):
+                        try:
+                            for i in numerical_features:
+                                std_before_random_replacement = np.round(data_pure[i].std(), 4)
+                                data_pick_random = data_pure[i].dropna().sample(data_pure[i].isnull().sum(),
+                                                                                random_state=0)
+                                data_pick_random.index = data_pure[data_pure[i].isnull()].index
+                                data_pure.loc[data_pure[i].isnull(), i] = data_pick_random
+                                std_after_random_replacement = np.round(data_pure[i].std(), 4)
+                                print(
+                                    "replaced the feature {} with random and the standard deviation befor applying random {} and after applying random is: {}".format(
+                                        i, std_before_random_replacement, std_after_random_replacement))
+                        except:
+                            raise Exception
 
 if __name__ == '__main__':
     app.debug = True
