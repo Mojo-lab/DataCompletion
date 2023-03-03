3f336c1dc4d07a40cd2affb5e19b9093d7b26d44
govardhan26 <govardhan_selvaraj@yahoo.com>
2023-03-03 22:39:35 +0530

    updated html and image files

--- dataCompletion.py
+++ dataCompletion.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
+@app.route('/login_page')
+def login_page():
+    return render_template('login_page.html')
+
+@app.route('/signup_page')
+def signup_page():
+    return render_template('signup_page.html')
 
 if __name__ == '__main__':
     app.debug = True
