50872d0c0528863bc52ac806c8bcdab66da377a7
govardhan26 <govardhan_selvaraj@yahoo.com>
2023-03-21 01:17:43 +0530

    updated files

--- config.py
+++
@@ -1,5 +0,0 @@
-sql_password = "Qwerty@123"
-secret_key = "apple"
-MAIL_USERNAME = 'selvarajgovardhan@gmail.com'
-MAIL_PASSWORD = 'vudlvsyuefrcgxtq'
-MAIL_Recepients = ['govardhan_selvaraj@yahoo.com']
